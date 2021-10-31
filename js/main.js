d89c21bf909ed4896f401cf135ff6b1c565c59d7
rubentxogg <rubentxogg@hotmail.com>
2021-10-31 12:54:04 +0100

    [ADD] Carrito de la compra

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ function inicializarFrutas() {
   // Verano
   let sandia = new FrutaVerano("Sandía", 1.35, 0, true, "Andalucía");
   let cereza = new FrutaVerano("Cereza", 4.75, 0, true, "Cataluña");
-  let uvas = new FrutaVerano("Uva", 0.84, 0, true, "Valencia");
+  let uvas = new FrutaVerano("Uvas", 0.84, 0, true, "Valencia");
   let coco = new FrutaVerano("Coco", 3.77, 0, false, "Yakarta");
   let fresa = new FrutaVerano("Fresa", 1.99, 0, true, "Andalucía");
 
@@ -67,33 +67,60 @@ function anadirCantidadKilosAFrutas() {
 
   const FRUTAS = {
     limon: () =>
-      frutas[0].setCantidadKg(frutas[0].getCantidadKg() + cantidadKg),
+      frutas[0].setCantidadKg(frutas[0].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[0].getNombre(), cantidadKg)),
+
     manzanaVerde: () =>
-      frutas[1].setCantidadKg(frutas[1].getCantidadKg() + cantidadKg),
+      frutas[1].setCantidadKg(frutas[1].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[1].getNombre(), cantidadKg)),
+
     manzanaRoja: () =>
-      frutas[2].setCantidadKg(frutas[2].getCantidadKg() + cantidadKg),
+      frutas[2].setCantidadKg(frutas[2].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[2].getNombre(), cantidadKg)),
+
     pina: () => 
-      frutas[3].setCantidadKg(frutas[3].getCantidadKg() + cantidadKg),
+      frutas[3].setCantidadKg(frutas[3].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[3].getNombre(), cantidadKg)),
+
     aguacate: () =>
-      frutas[4].setCantidadKg(frutas[4].getCantidadKg() + cantidadKg),
+      frutas[4].setCantidadKg(frutas[4].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[4].getNombre(), cantidadKg)),
+
     naranja: () =>
-      frutas[5].setCantidadKg(frutas[5].getCantidadKg() + cantidadKg),
+      frutas[5].setCantidadKg(frutas[5].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[5].getNombre(), cantidadKg)),
+
     kiwi: () => 
-      frutas[6].setCantidadKg(frutas[6].getCantidadKg() + cantidadKg),
+      frutas[6].setCantidadKg(frutas[6].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[6].getNombre(), cantidadKg)),
+
     banana: () =>
-      frutas[7].setCantidadKg(frutas[7].getCantidadKg() + cantidadKg),
+      frutas[7].setCantidadKg(frutas[7].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[7].getNombre(), cantidadKg)),
+
     papaya: () =>
-      frutas[8].setCantidadKg(frutas[8].getCantidadKg() + cantidadKg),
+      frutas[8].setCantidadKg(frutas[8].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[8].getNombre(), cantidadKg)),
+
     sandia: () =>
-      frutas[9].setCantidadKg(frutas[9].getCantidadKg() + cantidadKg),
+      frutas[9].setCantidadKg(frutas[9].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[9].getNombre(), cantidadKg)),
+
     cereza: () =>
-      frutas[10].setCantidadKg(frutas[10].getCantidadKg() + cantidadKg),
+      frutas[10].setCantidadKg(frutas[10].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[10].getNombre(), cantidadKg)),
+
     uvas: () => 
-      frutas[11].setCantidadKg(frutas[11].getCantidadKg() + cantidadKg),
+      frutas[11].setCantidadKg(frutas[11].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[11].getNombre(), cantidadKg)),
+
     coco: () => 
-      frutas[12].setCantidadKg(frutas[12].getCantidadKg() + cantidadKg),
+      frutas[12].setCantidadKg(frutas[12].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[12].getNombre(), cantidadKg)),
+      
     fresa: () =>
-      frutas[13].setCantidadKg(frutas[13].getCantidadKg() + cantidadKg),
+      frutas[13].setCantidadKg(frutas[13].getCantidadKg() + cantidadKg,
+      mostrarEnCarrito(frutas[13].getNombre(), cantidadKg)),
   };
 
   for (let i = 0; i < imagenesFrutas.length; i++) {
@@ -112,17 +139,23 @@ function anadirCantidadKilosAFrutas() {
         efectoFrutaClickError(this);
         alert("Debes introducir una cantidad mayor a 0");
       }
-      iniciarBotonCompra();
+      if (!comprobarCestaVacia(frutas)) iniciarBotonCompra(); 
     };
   }
 
   function iniciarBotonCompra() {
-    if (!comprobarCestaVacia(frutas)) {
-      BOTON_FIN_COMPRA.textContent = "¡Finalizar compra!";
-      BOTON_FIN_COMPRA.removeAttribute("disabled");
-      BOTON_FIN_COMPRA.removeAttribute("class", "botonDesactivado");
-      limpiarValue(RESUMEN_COMPRA);
-    }
+    BOTON_FIN_COMPRA.textContent = "¡Finalizar compra!";
+    BOTON_FIN_COMPRA.removeAttribute("disabled");
+    BOTON_FIN_COMPRA.removeAttribute("class", "botonDesactivado");
+    limpiarValue(RESUMEN_COMPRA);
+  }
+
+  function mostrarEnCarrito(nombre, cantidadKg){
+    const ASIDE_DCHA = document.getElementById("asideDcha");
+    let parrafo = document.createElement("p");
+
+    parrafo.textContent = `${nombre}: ${cantidadKg}`;
+    ASIDE_DCHA.append(parrafo);
   }
 }
 
